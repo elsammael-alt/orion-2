a821f68215fcd43f00d56bbaf14e87f5efc5d58c
elsammael-alt <elsammael@gmail.com>
2026-07-12 07:31:26 +0000

    Fix NotificationCompat: create notification channel for Orion foreground service

--- app/src/main/java/com/orion/app/service/OrionService.kt
+++ app/src/main/java/com/orion/app/service/OrionService.kt
@@ -17,6 +17,8 @@ import android.util.Log
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
